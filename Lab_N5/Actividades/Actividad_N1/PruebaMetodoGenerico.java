6f4c0ac2c38aa8197dee366e206ce83e37ce8ded
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-09-30 01:58:57 -0500

    Create PruebaMetodoGenerico.java

---
+++ Lab_N5/Actividades/Actividad_N1/PruebaMetodoGenerico.java
@@ -0,0 +1,69 @@
+
+
+import java.util.Scanner;
+
+class InvalidSubscriptException extends Exception {
+    public InvalidSubscriptException() {
+        super("(!) Indice de arreglo no valido");
+    }
+}
+public class PruebaMetodoGenerico { 
+  
+  public static <E> void imprimirArreglo (E[] arregloEntrada) {
+
+	  for (E elemento: arregloEntrada) {
+		  System.out.printf("%s", elemento);
+	      System.out.print(" ");
+
+	  }
+      System.out.println();
+
+  }
+
+  public static <E> void imprimirArreglo (E[] arregloEntrada, int subindiceInferior, int subindiceSuperior) throws InvalidSubscriptException {
+	  
+    if (subindiceInferior < 0 || subindiceSuperior >= arregloEntrada.length || subindiceInferior >= subindiceSuperior)  {
+      throw new InvalidSubscriptException();
+    }
+    
+    System.out.println("Límites válidos: " + subindiceInferior + " - " + subindiceSuperior);
+
+    for (int i = subindiceInferior; i <= subindiceSuperior; i++) {
+        System.out.printf("%s ", arregloEntrada[i]);
+      }
+      System.out.println();
+      
+      int cantRango = subindiceSuperior - subindiceInferior + 1;
+      System.out.println("Cantidad de elementos dentro del rango: " + cantRango);
+      
+  }
+
+  public static void main (String args[]) {
+    
+    Scanner scanner = new Scanner(System.in);
+    int subindiceInferior, subindiceSuperior;
+    
+    Integer[] arregloInteger = {1, 2, 3, 4};
+    Character[] arregloCharacter = { 'H', 'O', 'L', 'A' };
+    
+    
+    try {
+    
+    System.out.println( "\nIngrese el subindice inferior: " );
+    subindiceInferior = scanner.nextInt();
+    
+    System.out.println( "\nIngrese el subindice superior: " );
+    subindiceSuperior = scanner.nextInt();
+    
+    imprimirArreglo(arregloInteger, subindiceInferior, subindiceSuperior);
+    imprimirArreglo(arregloCharacter);
+
+  
+    
+    } catch (InvalidSubscriptException e) {
+      
+      System.out.println("Error: " + e.getMessage());
+      
+    } 
+  } 
+}
