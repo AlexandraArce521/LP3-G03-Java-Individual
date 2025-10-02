ad6082c4e86371eec28bdc178f6edc2668a2b3a8
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-10-01 21:18:39 -0500

    Update Main.java

--- Lab_N5/Ejercicios/Ejercicio_N1/Main.java
+++ Lab_N5/Ejercicios/Ejercicio_N1/Main.java
@@ -4,15 +4,12 @@ public class Main {
     public static void main(String[] args) {
       
       Par<Integer, String> par1 = new Par<>(2, "Hola!");
-      Par<Integer, String> par2 = new Par<>(3, "Adios");
       
       System.out.println(par1);
       
-      //par1.setPrimero(3);
+      par1.setPrimero(3);
       
       System.out.println(par1);
-      
-      System.out.println(par1.esIgual(par2));
-        
+              
   }
 }
