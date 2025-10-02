dabfc7d43371aa5a2be2dab9a66d652d3ca58bc0
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-10-01 23:30:26 -0500

    Update PruebaPar.java

--- Lab_N5/Ejercicios/Ejercicio_N3/PruebaPar.java
+++ Lab_N5/Ejercicios/Ejercicio_N3/PruebaPar.java
@@ -11,16 +11,31 @@ public class PruebaPar {
     
     public static void main(String[] args) {
       
+      Persona persona1 = new Persona(16, "Jorge");
+
+      Par<Integer, String> par0 = new Par<>(2, "Hola!");
       Par<Integer, String> par1 = new Par<>(2, "Hola!");
-      Par<Integer, String> par2 = new Par<>(3, "Adios");
+      Par<String, Integer> par2 = new Par<>("Java", 6);
+      Par<Double, Boolean> par3 = new Par<>(12.5, true);
+      Par<Persona, Integer> par4 = new Par<>(persona1, 3);
+
       
       System.out.println(par1);
-    
-      System.out.println(par1);
-        
+      System.out.println(par2);
+      System.out.println(par3);
+      System.out.println(par4);
+      
+      
       System.out.println(par1.esIgual(par2));
+      System.out.println(par0.esIgual(par1));
+      System.out.println(par3.esIgual(par4));
+
       
+      ImprimirObjeto.imprimirPar(par1);
       ImprimirObjeto.imprimirPar(par2);
+      ImprimirObjeto.imprimirPar(par3);
+      ImprimirObjeto.imprimirPar(par4);
+      
         
   }
 }
