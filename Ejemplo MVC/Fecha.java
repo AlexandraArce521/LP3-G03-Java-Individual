5611e6442e0fe2edccfb8540fc31ee9b3ff2e0dd
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-10-02 10:44:45 -0500

    Create Fecha.java

---
+++ Ejemplo MVC/Fecha.java
@@ -0,0 +1,15 @@
+public class Fecha {
+
+  protected int minuto;
+  protected int hora;
+  protected int dia;
+  protected int anio;
+  
+  public Fecha(int minuto, int hora, int dia, int anio) {
+    this.minuto = minuto;
+    this.hora = hora;
+    this.dia = dia;
+    this.anio = anio;
+  }
+
+}
