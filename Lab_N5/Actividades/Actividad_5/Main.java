366f28a1edd80449374ef772ea156db43796dc56
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-10-02 21:36:29 -0500

    Update Main.java

--- Lab_N5/Actividades/Actividad_5/Main.java
+++ Lab_N5/Actividades/Actividad_5/Main.java
@@ -7,7 +7,11 @@ public class Main {
 
       IgualGenerico comparar = new IgualGenerico(mascota, pi);
       
-      
+     IgualGenerico<String> obj2 = new IgualGenerico<>("Hola", "Mundo");
+    
+    System.out.println(obj2.esIgualA("Hola", "Hola")); // true
+    System.out.println(obj2.esIgualA("Hola", "Mundo")); // false
+     
       System.out.println("¿Es igual?: " + mascota +" y "+pi);
       System.out.println(comparar.esIgualA(mascota, pi));
 
