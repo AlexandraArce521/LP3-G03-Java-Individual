c44da94a7c1fae3e6aed11f00183bb8ae8a315b2
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-10-02 21:41:16 -0500

    Create Main.java

---
+++ Lab_N5/Actividades/Actividad_N4/Main.java
@@ -0,0 +1,19 @@
+public static void main(String[] args) throws Exception {
+    Pila<Integer> pila1 = new Pila<>(5);
+    Pila<Integer> pila2 = new Pila<>(5);
+
+    pila1.push(10);
+    pila1.push(20);
+    pila1.push(30);
+
+    pila2.push(10);
+    pila2.push(20);
+    pila2.push(30);
+
+    System.out.println(pila1.esIgual(pila2)); // true
+
+    pila2.pop();
+    pila2.push(40);
+
+    System.out.println(pila1.esIgual(pila2)); // false
+}
