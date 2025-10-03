2940cf337b69f01babc9c6de35fc537ee464c617
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-10-02 21:43:38 -0500

    Update Main.java

--- Lab_N5/Actividades/Actividad_N4/Main.java
+++ Lab_N5/Actividades/Actividad_N4/Main.java
@@ -1,4 +1,4 @@
-public static void main(String[] args) throws Exception {
+public static void main(String[] args) {
     Pila<Integer> pila1 = new Pila<>(5);
     Pila<Integer> pila2 = new Pila<>(5);
 
