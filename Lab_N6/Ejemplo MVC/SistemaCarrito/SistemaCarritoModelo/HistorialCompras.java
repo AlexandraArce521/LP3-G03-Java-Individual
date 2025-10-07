1cfda0f3b1b4bf2baca1bcd6970c902c12e07933
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-10-07 01:47:07 -0500

    Update HistorialCompras.java

--- Lab_N6/Ejemplo MVC/SistemaCarrito/SistemaCarritoModelo/HistorialCompras.java
+++ Lab_N6/Ejemplo MVC/SistemaCarrito/SistemaCarritoModelo/HistorialCompras.java
@@ -5,8 +5,8 @@ public class HistorialCompras {
   public void historialCompras(Carrito carrito) {
     ArrayList<Producto> lista = carrito.getCarrito();
 
-    System.out.println("Historial de compras");
-    System.out.println("________________________");
+    System.out.println("__________________________");
+    System.out.println("Historial de compras:\n__________________________\n");
 
     for (int i = 0; i < lista.size(); i++) {
         System.out.println((i + 1) + ". " + lista.get(i).toString());
