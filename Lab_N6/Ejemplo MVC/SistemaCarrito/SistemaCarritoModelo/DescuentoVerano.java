d8eb9f1c4954c759343702db23ecd5cf6328156d
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-10-07 01:44:47 -0500

    Create DescuentoVerano.java

---
+++ Lab_N6/Ejemplo MVC/SistemaCarrito/SistemaCarritoModelo/DescuentoVerano.java
@@ -0,0 +1,18 @@
+public class DescuentoVerano implements AplicadorDescuento
+{
+    
+	@Override
+    public double descuentoFijo(Carrito carrito) {
+      
+        double total = carrito.totalPrecio();
+        double descuento = 0;
+
+
+        if (total > 1) {
+            descuento = total * 0.10;
+        }
+
+        return total - descuento;
+    }
+    
+}
