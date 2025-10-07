8766da2589f31a8cad749c67ccc7e23d0ffc8b3b
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-10-07 01:45:04 -0500

    Create HistorialCompras.java

---
+++ Lab_N6/Ejemplo MVC/SistemaCarrito/SistemaCarritoModelo/HistorialCompras.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class HistorialCompras {
+  
+  public void historialCompras(Carrito carrito) {
+    ArrayList<Producto> lista = carrito.getCarrito();
+
+    System.out.println("Historial de compras");
+    System.out.println("________________________");
+
+    for (int i = 0; i < lista.size(); i++) {
+        System.out.println((i + 1) + ". " + lista.get(i).toString());
+    }
+    
+  }
+
+}
