bebf60aec5d036cd355b434dfdd74b04beb85845
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-10-27 08:18:16 -0500

    Create CalculadoraVista.java

---
+++ MisEjemplos/MVC/CalculadoraVista.java
@@ -0,0 +1,8 @@
+public class CalculadoraVista  {
+    public void mostrarResultado(double resultado) {
+        System.out.println("= " + resultado);
+    }
+    public void mostrarOperandos(double a, double b) {
+        System.out.println("a = " + a + ", b = " + b);
+    } 
+}
