0f56cd65ee9c7f9951e99a4598acc587220f84b4
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-12-03 21:44:46 -0500

    Create AireAcondicionado.java

---
+++ MisEjemplos/Patrones de diseño/Strategy/Command/AireAcondicionado.java
@@ -0,0 +1,10 @@
+public class AireAcondicionado {
+
+    public void encender() {
+        System.out.println("El aire acondicionado ha sido encendido");
+    }
+
+    public void apagar() {
+        System.out.println("El aire acondicionado ha sido apagado");
+    }
+}
