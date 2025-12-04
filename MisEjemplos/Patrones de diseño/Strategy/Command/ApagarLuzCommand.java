09b11349a52417230efef49c125bca1b5fbad414
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-12-03 21:49:11 -0500

    Create ApagarLuzCommand.java

---
+++ MisEjemplos/Patrones de diseño/Strategy/Command/ApagarLuzCommand.java
@@ -0,0 +1,18 @@
+public class ApagarLuzCommand implements Command {
+
+    private Luz luz;
+
+    public ApagarLuzCommand(Luz luz) {
+        this.luz = luz;
+    }
+
+    @Override
+    public void ejecutar() {
+        luz.apagar();
+    }
+
+    @Override
+    public void deshacer() {
+        luz.encender();
+    }
+}
