b7d82e90d95b7b23ebef34231e3bbcc142216295
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-12-03 21:25:28 -0500

    Create CalculadoraDePrecios.java

---
+++ MisEjemplos/Patrones de diseño/Strategy/CalculadoraDePrecios.java
@@ -0,0 +1,11 @@
+class CalculadoraDePrecios {
+  private DescuentoEstrategia descuentoEstrategia;
+  
+  public void setDescuentoPrecio(DescuentoEstrategia descuentoEstrategia) {
+    this.descuentoEstrategia = descuentoEstrategia;
+  }
+  
+  public double calcularPrecio(double precio) {
+    return descuentoEstrategia.applyDiscount(precio);
+  }
+}
