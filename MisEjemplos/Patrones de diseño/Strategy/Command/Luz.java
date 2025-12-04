ab25641dbe53f0313433e795c6ec9d224d43290e
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-12-03 21:43:49 -0500

    Create Luz.java

---
+++ MisEjemplos/Patrones de diseño/Strategy/Command/Luz.java
@@ -0,0 +1,10 @@
+public class Luz {
+
+    public void encender() {
+        System.out.println("La luz ha sido encendida");
+    }
+
+    public void apagar() {
+        System.out.println("La luz ha sido apagada");
+    }
+}
