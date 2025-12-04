0aea3b4d3533a1f76da20cc83c9f64cd33906c1b
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-12-03 21:44:19 -0500

    Create Ventilador.java

---
+++ MisEjemplos/Patrones de diseño/Strategy/Command/Ventilador.java
@@ -0,0 +1,11 @@
+public class Ventilador {
+
+    public void encender() {
+        System.out.println("El ventilador ha sido encendido");
+    }
+
+    public void apagar() {
+        System.out.println("El ventilador ha sido apagado");
+    }
+}
+
