8efdd29ebf0a273e17c24dc84505f95622a85612
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-12-03 21:29:56 -0500

    Create Producto.java

---
+++ MisEjemplos/Patrones de diseño/Strategy/Producto.java
@@ -0,0 +1,26 @@
+public class Producto {
+  public String nombre;
+  public double precio;
+
+  public Producto(String nombre, double precio) {
+    this.nombre = nombre;
+    this.precio = precio,
+  }
+  
+  public String getNombre() {
+    return nombre;
+  }
+  
+  public double getPrecio() {
+    return precio;
+  }
+  
+  public void setNombre(String n) {
+    nombre = n;
+  }
+  
+  public void setPrecio(double p) {
+    precio = p;
+  }
+
+}
