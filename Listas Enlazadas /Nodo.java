76e2eaab3fef8fc8ff89d3bc264e2088d997c897
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2026-04-28 01:24:02 -0500

    Create Nodo.java

---
+++ Listas Enlazadas /Nodo.java
@@ -0,0 +1,15 @@
+public class Nodo <T> {
+    public T valor;
+    public Nodo<T> next;
+    
+    public Nodo(T valor){
+		this.valor = valor;
+		this.next = null;
+	}
+	
+    @Override
+    public String toString() {
+        return String.valueOf(valor);
+    }
+    	
+}
