b27f6d2d4f78c6efd01b876d97914db9c0524a86
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2026-04-30 11:11:44 -0500

    Update PilaEnlazada.java

--- PilasEnlazadas/PilaEnlazada.java
+++ PilasEnlazadas/PilaEnlazada.java
@@ -1,37 +1,38 @@
-public class PilaEnlazada <T> implements Pila {
-    
+public class PilaEnlazada<T> implements Pila<T> {
+
     protected Nodo<T> cabeza;
     protected int tamanio;
-    
-    public PilaEnlazada(Nodo cabeza, int tamanio) {
+
+    public PilaEnlazada() {
         this.cabeza = null;
         this.tamanio = 0;
     }
-    
+
     public void push(T x) {
         Nodo<T> nuevoNodo = new Nodo<>(x);
         nuevoNodo.next = cabeza;
         cabeza = nuevoNodo;
         tamanio++;
     }
-    
-    public <T> T pop() {
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("La pila está vacía");
+        }
         T dato = cabeza.valor;
         cabeza = cabeza.next;
-        return dato;
-        
-    }
-    
-    public <T> T top() {
-        T dato = cabeza.valor;
+        tamanio--;
         return dato;
     }
-    
-    public boolean isEmpty() {
-        if (cabeza == null) {
-            return false;
+
+    public T top() {
+        if (isEmpty()) {
+            throw new RuntimeException("La pila está vacía");
         }
-        
-        return true;
+        return cabeza.valor;
+    }
+
+    public boolean isEmpty() {
+        return cabeza == null;
     }
 }
